42c8c71ea59ff48b0195a6c42ae2aea87ab5ea54
toracle <toracle@gmail.com>
2014-12-19 02:28:41 +0900

    GUI 모드일 때는 escaped keycode를 사용하지 않도록 변경함.

--- init-loader/01_windmove.el
+++ init-loader/01_windmove.el
@@ -1,14 +1,19 @@
-;; Windmove                                                                                                                                      
+;; Windmove
 
-(define-key input-decode-map "\e\eOA" [S-up])
-(define-key input-decode-map "\e\eOB" [S-down])
-(define-key input-decode-map "\e\eOC" [S-right])
-(define-key input-decode-map "\e\eOD" [S-left])
+(if (display-graphic-p)
+    nil
+  (progn
+    (define-key input-decode-map "\e\eOA" [S-up])
+    (define-key input-decode-map "\e\eOB" [S-down])
+    (define-key input-decode-map "\e\eOC" [S-right])
+    (define-key input-decode-map "\e\eOD" [S-left])))
 
 (when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
+  (if (display-graphic-p)
+      (windmove-default-keybindings 'meta)
+      (windmove-default-keybindings)))
 
-(global-set-key (kbd "C-c <left>")  'windmove-left)
-(global-set-key (kbd "C-c <right>") 'windmove-right)
-(global-set-key (kbd "C-c <up>")    'windmove-up)
-(global-set-key (kbd "C-c <down>")  'windmove-down)
+;(global-set-key (kbd "C-c <left>")  'windmove-left)
+;(global-set-key (kbd "C-c <right>") 'windmove-right)
+;(global-set-key (kbd "C-c <up>")    'windmove-up)
+;(global-set-key (kbd "C-c <down>")  'windmove-down)

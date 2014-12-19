49f9b7485313b078771191d737d2e215b2767f80
toracle <toracle@gmail.com>
2014-12-19 11:22:25 +0900

    ample-theme 사용하도록 설정함.
    MS윈도우 GUI에서는 폰트를 나눔고딕 사용하도록 함.

--- Cask
+++ Cask
@@ -26,7 +26,7 @@
 (depends-on "tabbar-ruler")
 (depends-on "virtualenv")
 (depends-on "yasnippet")
-
+(depends-on "ample-theme")
 
 ;; (depends-on "dash")
 ;; (depends-on "drag-stuff")
--- init-loader/05_gui.el
+++ init-loader/05_gui.el
@@ -1,2 +1,16 @@
 (if (display-graphic-p)
-    (tool-bar-mode -1))
+    (progn
+      (tool-bar-mode -1)
+      (ample-theme)
+      )
+  )
+
+
+(if (and (display-graphic-p) (eq system-type 'windows-nt))
+    (let ((fontset "fontset-default"))
+      (set-face-font 'default "NanumGothicCoding")
+      (set-fontset-font fontset 'hangul '("NanumGothicCoding" . "unicode-bmp"))
+      (set-face-attribute 'default nil :font fontset :height 105)
+      (add-text-properties (point-min) (point-max) '(line-spacing 0.25 line-height 1.0))
+      )
+  )

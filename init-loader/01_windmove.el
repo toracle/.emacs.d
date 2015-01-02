6d879c667b3e92b815ea00b46af4076a23951d07
Jeongsoo Park <toracle@gmail.com>
2015-01-02 18:55:31 +0900

    python mode에서 windmove가 사용하는 단축키와 충돌하는 경우가 있어서, C-c <arrow> key binding을 추가함.

--- init-loader/01_windmove.el
+++ init-loader/01_windmove.el
@@ -13,7 +13,7 @@
       (windmove-default-keybindings 'meta)
       (windmove-default-keybindings)))
 
-;(global-set-key (kbd "C-c <left>")  'windmove-left)
-;(global-set-key (kbd "C-c <right>") 'windmove-right)
-;(global-set-key (kbd "C-c <up>")    'windmove-up)
-;(global-set-key (kbd "C-c <down>")  'windmove-down)
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)

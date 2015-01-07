37b01428cc0d268e942710ca8d43555da5d04be3
Jeongsoo Park <toracle@gmail.com>
2015-01-07 13:33:38 +0900

    C-c g에 magit status를 연결함.

--- init-loader/07_magit.el
+++ init-loader/07_magit.el
@@ -3,3 +3,5 @@
       (setq exec-path (add-to-list 'exec-path "C:/Program Files (x86)/Git/bin"))
       (setq magit-git-executable "C:/Program Files (x86)/Git/bin/git.exe")
       (setenv "PATH" (concat "C:\\Program Files (x86)\\Git\\bin;" (getenv "PATH")))))
+
+(global-set-key (kdb "C-c g") 'magit-status)

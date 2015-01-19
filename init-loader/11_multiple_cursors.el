4adb5271a7652b2398709e5c033592c09cd18ee6
Jeongsoo Park <toracle@gmail.com>
2015-01-19 13:08:04 +0900

    expand-region과 multiple cursor의 key bind를 변경함.

--- init-loader/11_multiple_cursors.el
+++ init-loader/11_multiple_cursors.el
@@ -1,7 +1,7 @@
 (require 'expand-region)
-(global-set-key (kbd "C-@") 'er/expand-region)
+(global-set-key (kbd "C-c C-m 2") 'er/expand-region)
 
 (require 'multiple-cursors)
-(global-set-key (kbd "C-c .") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c ,") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c >") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c C-m .") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C-m ,") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-m a") 'mc/mark-all-like-this)

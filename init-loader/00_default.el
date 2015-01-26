f40d464ae14343b85bf04a94fd4e8083144a2501
Jeongsoo Park <toracle@gmail.com>
2015-01-26 13:03:52 +0900

    theme signature 업데이트함.

--- init-loader/00_default.el
+++ init-loader/00_default.el
@@ -9,7 +9,7 @@
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-(setq custom-safe-themes '("b71d5d49d0b9611c0afce5c6237aacab4f1775b74e513d8ba36ab67dfab35e5a" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default))
+(setq custom-safe-themes '("e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" "b71d5d49d0b9611c0afce5c6237aacab4f1775b74e513d8ba36ab67dfab35e5a" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default))
 
 (require 'smart-mode-line)
 (sml/setup)

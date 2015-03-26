f4c965362699893058fe8db6253f8a21b2d0c42d
Jeongsoo Park <toracle@gmail.com>
2015-03-26 18:28:51 +0900

    multiple cursor에 대한 단축키 변경. 기존의 C-m은 엔터키와 동일하기 때문에.

--- init-loader/11_multiple_cursors.el
+++ init-loader/11_multiple_cursors.el
@@ -2,6 +2,6 @@
 (global-set-key (kbd "M-2") 'er/expand-region)
 
 (require 'multiple-cursors)
-(global-set-key (kbd "C-c C-m C-.") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c C-m C-,") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-m C-a") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c m .") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c m ,") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c m a") 'mc/mark-all-like-this)

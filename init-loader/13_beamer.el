23fae9e74c28ab0bd605682637d831f8e5f3ce9f
toracle <toracle@gmail.com>
2015-06-21 00:45:01 +0900

    add a beamer related configuration

---
+++ init-loader/13_beamer.el
@@ -0,0 +1,7 @@
+(require 'ox-latex)
+(add-to-list 'org-latex-classes
+             '("beamer"
+               "\\documentclass\[presentation\]\{beamer\}"
+               ("\\section\{%s\}" . "\\section*\{%s\}")
+               ("\\subsection\{%s\}" . "\\subsection*\{%s\}")
+               ("\\subsubsection\{%s\}" . "\\subsubsection*\{%s\}")))

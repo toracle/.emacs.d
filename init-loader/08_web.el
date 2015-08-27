2ed49b4f330f0b19cf08a93e1de5da32a9366834
toracle <toracle@gmail.com>
2015-08-27 23:13:00 +0900

    web-mode에서 C-c / 키로 태그를 닫는 단축키 설정함

--- init-loader/08_web.el
+++ init-loader/08_web.el
@@ -6,3 +6,10 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (local-set-key (kbd "C-c /")  'web-mode-element-close)
+  )
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)

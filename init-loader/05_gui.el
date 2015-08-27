b8611512a9acbe57b1648548004f9c7d9afc62c1
Jeongsoo Park <toracle@gmail.com>
2015-08-27 14:11:49 +0900

    Font 설정 조정함
     - line-spacing을 비례값에서 절대값으로 변경함.
     - 나눔고딕 폰트 크기 중에서 영문 폰트의 딱 2배가 되는 값들이 있음. 그런 값으로 변경함.

--- init-loader/05_gui.el
+++ init-loader/05_gui.el
@@ -6,7 +6,7 @@
       (progn
 	(tool-bar-mode -1)
 	(scroll-bar-mode -1)
-	(setq-default line-spacing 0.20)
+	(setq-default line-spacing 4)
 	)
 
       (when (eq system-type 'windows-nt)
@@ -19,8 +19,8 @@
 	(setq mac-command-modifier 'meta)
 	(setq mac-right-option-modifier 'control)
 	
-	(set-frame-font (format "%s:pixelsize=%d" fontname 14) t)
+;	(set-frame-font (format "%s:pixelsize=%d" fontname 14) t)
 	(set-fontset-font (frame-parameter nil 'font) charset
-			  (font-spec :family fontname :size 13))
+			  (font-spec :family fontname :size 12))
 	)
       ))

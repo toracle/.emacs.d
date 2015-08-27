37a81db1c41d3a672630ebc47ada2d91c6bec999
Jeongsoo Park <toracle@gmail.com>
2015-08-27 14:56:37 +0900

    if문 대신 when문을 사용하여 문법을 간소화함

--- init-loader/07_magit.el
+++ init-loader/07_magit.el
@@ -1,8 +1,8 @@
-(if (eq system-type 'windows-nt)
-    (progn
-      (setq exec-path (add-to-list 'exec-path "C:/Program Files (x86)/Git/bin"))
-      (setq magit-git-executable "C:/Program Files (x86)/Git/bin/git.exe")
-      (setenv "PATH" (concat "C:\\Program Files (x86)\\Git\\bin;" (getenv "PATH")))))
+(when
+    (eq system-type 'windows-nt)
+  (setq exec-path (add-to-list 'exec-path "C:/Program Files (x86)/Git/bin"))
+  (setq magit-git-executable "C:/Program Files (x86)/Git/bin/git.exe")
+  (setenv "PATH" (concat "C:\\Program Files (x86)\\Git\\bin;" (getenv "PATH"))))
 
 (global-set-key (kbd "C-c g") 'magit-status)
 (setq magit-last-seen-setup-instructions "1.4.0")

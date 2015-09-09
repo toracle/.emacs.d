87fc1a186135d37179f8d2dbd69de88d3a5df850
Jeongsoo Park <toracle@gmail.com>
2015-09-09 16:01:51 +0900

    Elpy에서 사용하던 C-c C-s 키에 projectile의 grep 명령을 bind함

--- init-loader/03_python.el
+++ init-loader/03_python.el
@@ -15,5 +15,6 @@
    (setq jedi:complete-on-dot t)
    (local-set-key (kbd "M-.") 'jedi:goto-definition)
    (local-set-key (kbd "M-*") 'jedi:goto-definition-pop-marker)
+   (local-set-key (kbd "C-c C-s") 'projectile-grep)
    )
  )

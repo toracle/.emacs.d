67a6f49fedcdcde894117364e3c5740a0c69d211
Jeongsoo Park <toracle@gmail.com>
2015-09-24 14:36:03 +0900

    Resolve an hook acculating problem

--- init-loader/09_projectile.el
+++ init-loader/09_projectile.el
@@ -1,18 +1,10 @@
 (require 'projectile)
 (projectile-global-mode)
 
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
-
-
-;(setq projectile-switch-project-action 'neotree-projectile-action)
-
-(add-to-list 'projectile-project-root-files ".svn")
-(add-to-list 'projectile-project-root-files ".git")
-(add-to-list 'projectile-project-root-files ".projectile")
-
-
-(add-hook
- 'projectile-mode-hook
- (lambda ()
-   (local-set-key (kbd "C-c C-s") 'projectile-grep)))
+(progn
+   (helm-projectile-on)
+   (add-to-list 'projectile-project-root-files ".svn")
+   (add-to-list 'projectile-project-root-files ".git")
+   (add-to-list 'projectile-project-root-files ".projectile")
+   (setq projectile-completion-system 'helm)
+   (local-set-key (kbd "C-c C-s") 'projectile-grep))

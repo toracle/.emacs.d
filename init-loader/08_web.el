e46c740eb5b51f9f53b0339a320372b05ac86344
Jeongsoo Park <toracle@gmail.com>
2015-10-05 12:01:21 +0900

    Make html works with web-mode

--- init-loader/08_web.el
+++ init-loader/08_web.el
@@ -1,4 +1,5 @@
 (require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))

d6cbd92d60dfaf6a0ac2837cad561524146363d5
Jeongsoo Park <toracle@gmail.com>
2015-10-12 17:23:16 +0900

    Use boundp for check pyvenv-virtual-env is defined or not
    Python setup haven't applied due to an error during check
    pyvenv-virtual-env variable is defined or not. Use boundp for those
    case.

--- init-loader/03_python.el
+++ init-loader/03_python.el
@@ -1,21 +1,21 @@
-(add-hook
- 'python-mode-hook
- (lambda ()
-   (setq indent-tabs-mode nil)
-   (flymake-mode -1)
-   (setq flycheck-pylintrc
-	 (concat
-	  (file-name-as-directory
-	   (concat user-emacs-directory "init-loader"))
-	  "pylintrc"))
-   (when (not (null pyvenv-virtual-env))
-     (setq flycheck-python-pylint-executable (concat pyvenv-virtual-env "bin/pylint")))
-   (flycheck-select-checker 'python-pylint)
-   (jedi:setup)
-   (company-mode t)
-   (add-to-list 'company-backends 'company-jedi)
-   (setq jedi:complete-on-dot t)
-   (local-set-key (kbd "M-.") 'jedi:goto-definition)
-   (local-set-key (kbd "M-*") 'jedi:goto-definition-pop-marker)
-   (local-set-key (kbd "C-c C-d") 'jedi:show-doc)
-   ))
+(defun toracle-setup-python ()
+  (setq indent-tabs-mode nil)
+  (flymake-mode -1)
+  (setq flycheck-pylintrc
+	(concat (file-name-as-directory (concat user-emacs-directory "init-loader"))
+		"pylintrc"))
+  (when (and (boundp 'pyvenv-virtual-env)
+             (not (null pyvenv-virtual-env)))
+    (setq flycheck-python-pylint-executable
+          (concat pyvenv-virtual-env "bin/pylint")))
+  (flycheck-select-checker 'python-pylint)
+  (jedi:setup)
+  (setq jedi:complete-on-dot t)
+  (company-mode t)
+  (add-to-list 'company-backends 'company-jedi)
+  (local-set-key (kbd "M-.") 'jedi:goto-definition)
+  (local-set-key (kbd "M-*") 'jedi:goto-definition-pop-marker)
+  (local-set-key (kbd "C-c C-d") 'jedi:show-doc)
+  )
+
+(add-hook 'python-mode-hook 'toracle-setup-python)

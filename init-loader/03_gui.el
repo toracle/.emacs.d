34ce9929d5eea312c535ae0bc404137d7641ffca
Jeongsoo Park <toracle@gmail.com>
2016-05-18 19:15:22 +0900

    add font in/decrease functions and bind those to global key

--- init-loader/03_gui.el
+++ init-loader/03_gui.el
@@ -2,12 +2,23 @@
 
 ;;; Code:
 
-(defun my-set-default-font (fontname size)
+(defun toracle-set-default-font (fontname size)
+  (setq toracle-default-font-family fontname)
+  (setq toracle-default-font-size size)
   (set-frame-font (format "%s:pixelsize=%d" fontname size) t)
   (set-fontset-font t
 		    'unicode-bmp
 		    (font-spec :family fontname :size size)))
 
+(defun toracle-increase-font-size ()
+  (interactive)
+  (setq toracle-default-font-size (+ toracle-default-font-size 2))
+  (toracle-set-default-font toracle-default-font-family toracle-default-font-size))
+
+(defun toracle-decrease-font-size ()
+  (interactive)
+  (setq toracle-default-font-size (- toracle-default-font-size 2))
+  (toracle-set-default-font toracle-default-font-family toracle-default-font-size))
 
 (when (display-graphic-p)
   (tool-bar-mode -1)
@@ -18,6 +29,12 @@
     (setq mac-command-modifier 'meta)
     (setq mac-option-modifier 'super))
 
-  (my-set-default-font "D2Coding" 14))
+  (defvar toracle-default-font-size 14)
+  (defvar toracle-default-font-family "D2Coding")
+
+  (global-set-key (kbd "M-_") 'toracle-decrease-font-size)
+  (global-set-key (kbd "M-+") 'toracle-increase-font-size)
+  
+  (toracle-set-default-font "D2Coding" 14))
 
 ;;; 05_gui.el ends here

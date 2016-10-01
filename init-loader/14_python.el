ada75e2d64f4a8b86684cc0d87b7a85acf950d53
Jeongsoo Park <toracle@gmail.com>
2016-10-01 06:40:45 +0000

    apply anaconda mode

--- init-loader/14_python.el
+++ init-loader/14_python.el
@@ -7,28 +7,21 @@
 (use-package pyvenv
   :ensure t)
 
-(use-package company-jedi
+(use-package anaconda-mode
+  :ensure t
+  :bind (:map python-mode-map
+	      ("C-c C-d" . anaconda-mode-show-doc))
+  :config (progn
+	    (add-hook 'python-mode-hook 'anaconda-mode)))
+
+(use-package company-anaconda
   :ensure t
   :config (progn
-	    (jedi:setup)
-	    (setq jedi:complete-on-dot t)
-	    (add-to-list 'company-backends 'company-jedi)))
+	    (add-to-list 'company-backends 'company-anaconda)))
 
 (defun toracle-setup-python ()
   (setq indent-tabs-mode nil)
-  (flymake-mode -1)
-  (setq flycheck-pylintrc
-	(concat (file-name-as-directory (concat user-emacs-directory "init-loader"))
-		"pylintrc"))
-  (when (and (boundp 'pyvenv-virtual-env)
-             (not (null pyvenv-virtual-env)))
-    (setq flycheck-python-pylint-executable
-          (concat pyvenv-virtual-env "bin/pylint")))
-  (flycheck-select-checker 'python-pylint)
-  (company-mode t)
   (add-to-list 'grep-find-ignored-directories "build")
-  (local-set-key (kbd "M-.") 'jedi:goto-definition)
-  (local-set-key (kbd "M-*") 'jedi:goto-definition-pop-marker)
-  (local-set-key (kbd "C-c C-d") 'jedi:show-doc))
+  )
 
 (add-hook 'python-mode-hook 'toracle-setup-python)

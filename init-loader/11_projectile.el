342fb7aa2e411bf1a4dd0a83a27002b5de11c10f
Jeongsoo Park <toracle@gmail.com>
2016-10-04 12:22:32 +0900

    add setup.py to projectile root file

--- init-loader/11_projectile.el
+++ init-loader/11_projectile.el
@@ -5,5 +5,6 @@
    (add-to-list 'projectile-project-root-files ".svn")
    (add-to-list 'projectile-project-root-files ".git")
    (add-to-list 'projectile-project-root-files ".projectile")
+   (add-to-list 'projectile-project-root-files "setup.py")
    (setq projectile-completion-system 'helm)
    (local-set-key (kbd "C-c C-s") 'projectile-grep))

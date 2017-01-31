95ea66cb632ff666d0af4f5b2d741e55b6db358f
Jeongsoo Park <toracle@gmail.com>
2017-01-31 21:06:01 +0900

    add vue supports

--- init-loader/16_web.el
+++ init-loader/16_web.el
@@ -10,14 +10,17 @@
 	    (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 	    (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 	    (add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+	    (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 	    (add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))))
 
 (defun my-web-mode-hook ()
   "Hooks for Web mode."
-  (setq web-mode-markup-indent-offset 4)
-  (setq web-mode-css-indent-offset 4)
-  (setq web-mode-code-indent-offset 4)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
   (setq web-mode-enable-engine-detection t)
+  (setq web-mode-style-padding 0)
+  (setq web-mode-script-padding 0)
   (local-set-key (kbd "C-c /")  'web-mode-element-close)
   )
 

0780c37c8d05421bffcf83064af54a7b8efda450
Jeongsoo Park <toracle@gmail.com>
2017-04-11 19:16:22 -0700

    remove unnecessary codes

--- init-loader/11_projectile.el
+++ init-loader/11_projectile.el
@@ -6,6 +6,4 @@
 	    (add-to-list 'projectile-project-root-files ".git")
 	    (add-to-list 'projectile-project-root-files ".projectile")
 	    (add-to-list 'projectile-project-root-files "setup.py")
-	    (setq projectile-completion-system 'helm)
-	    (add-hook 'python-mode-hook (lambda () (projectile-mode t))))
-  :bind (("C-c C-s" . projectile-grep)))
+	    (setq projectile-completion-system 'helm)))

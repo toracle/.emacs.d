e0cd1a44ba89406484e13607d823db11b6e6734c
Jeongsoo Park <toracle@gmail.com>
2017-05-11 14:24:15 +0900

    comment out window-purpose mode; it's hard to use

--- init-loader/01_window.el
+++ init-loader/01_window.el
@@ -19,22 +19,35 @@
 
 (winner-mode 1)
 
+(defun toggle-window-dedicated ()
+  "Toggle whether the current active window is dedicated or not"
+  (interactive)
+  (message 
+   (if (let (window (get-buffer-window (current-buffer)))
+         (set-window-dedicated-p window 
+                                 (not (window-dedicated-p window))))
+       "Window '%s' is dedicated"
+     "Window '%s' is undedicated")
+   (current-buffer)))
+
+(global-set-key (kbd "C-c , .") 'toggle-window-dedicated)
+
 (defun purpose/init ()
   (add-to-list 'purpose-user-regexp-purposes '("magit:.*" . magit))
   (purpose-compile-user-configuration)
   (purpose-mode 1))
 
-(use-package window-purpose
-  :ensure t
-  :config (purpose/init))
-
 (use-package ivy
   :ensure t
   :config (ivy-mode 1))
 
-(use-package ivy-purpose
-  :ensure t
-  :config (ivy-purpose-setup)
-  :bind (("C-x b" . ivy-purpose-switch-buffer-with-purpose)))
+;; (use-package window-purpose
+;;   :ensure t
+;;   :config (purpose/init))
+
+;; (use-package ivy-purpose
+;;   :ensure t
+;;   :config (ivy-purpose-setup)
+;;   :bind (("C-x b" . ivy-purpose-switch-buffer-with-purpose)))
 
 ;;; 01_window.el ends here

8c79923d5207341db1705bef7c593fc0c1ecd618
Jeongsoo Park <toracle@gmail.com>
2019-05-17 12:23:13 +0900

    use default windmove key bindings

--- init-loader/01_window.el
+++ init-loader/01_window.el
@@ -5,10 +5,10 @@
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings 'meta))
 
-(global-set-key (kbd "C-c <left>")  'windmove-left)
-(global-set-key (kbd "C-c <right>") 'windmove-right)
-(global-set-key (kbd "C-c <up>")    'windmove-up)
-(global-set-key (kbd "C-c <down>")  'windmove-down)
+;; (global-set-key (kbd "C-c <left>")  'windmove-left)
+;; (global-set-key (kbd "C-c <right>") 'windmove-right)
+;; (global-set-key (kbd "C-c <up>")    'windmove-up)
+;; (global-set-key (kbd "C-c <down>")  'windmove-down)
 
 (global-set-key (kbd "M-L") 'enlarge-window-horizontally)
 (global-set-key (kbd "M-H") 'shrink-window-horizontally)

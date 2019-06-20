1a3d8b7fdb9e0cc7af1cb4fcca372aa811929df1
Jeongsoo Park <toracle@gmail.com>
2019-06-20 18:27:59 +0900

    replace C-x o with hydra key mapping, add hydra-switchpframe

--- init-loader/23_hydra.el
+++ init-loader/23_hydra.el
@@ -1,9 +1,14 @@
 (use-package hydra
   :ensure t)
 
-(defhydra hydra-windmove (global-map "C-x p")
+(defhydra hydra-windmove (global-map "C-x o")
   "windmove"
   ("j" windmove-left "left")
   ("l" windmove-right "right")
   ("i" windmove-up "up")
   ("k" windmove-down "down"))
+
+
+(defhydra hydra-switch-frame (global-map "C-x C-o")
+  "switch-frame"
+  ("o" other-frame "other"))

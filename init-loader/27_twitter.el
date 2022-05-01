16bb197d21933103bce0f9a0542774be7643eb14
Jeongsoo Park <toracle@gmail.com>
2022-05-01 22:47:10 +0900

    add mode hook, and set favorite key binding

--- init-loader/27_twitter.el
+++ init-loader/27_twitter.el
@@ -1,5 +1,7 @@
 (use-package twittering-mode
   :ensure t
   :config (progn
-            (setq twittering-timer-interval 300)))
-
+            (add-hook 'twittering-mode-hook
+                      (lambda ()
+                        (setq twittering-timer-interval 300)
+                        (local-set-key (kbd "s") 'twittering-favorite)))))

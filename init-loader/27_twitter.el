ec5998d8c59f22a91f358b3c5404a80b6788b0aa
Jeongsoo Park <toracle@gmail.com>
2022-05-11 09:35:24 +0900

    update twittering-status-format

--- init-loader/27_twitter.el
+++ init-loader/27_twitter.el
@@ -4,4 +4,16 @@
             (add-hook 'twittering-mode-hook
                       (lambda ()
                         (setq twittering-timer-interval 300)
+                        (setq twittering-icon-mode t)
+                        (setq twittering-status-format "%RT{%FACE[bold]{RT}}%i %S,  %@:
+
+ %T
+// from %f%L%r%R%QT{
+   +----
+   %i %S,  %@:
+   %T
+   // from %f%L%r%R
+   +----}
+
+ ")
                         (local-set-key (kbd "s") 'twittering-favorite)))))

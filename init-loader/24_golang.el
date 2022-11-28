e6bf7fac751790f1946762d67153a5e0a045fcdb
Jeongsoo Park <toracle@gmail.com>
2022-11-29 02:04:18 +0900

    use lsp-mode for go-mode

--- init-loader/24_golang.el
+++ init-loader/24_golang.el
@@ -1,2 +1,16 @@
 (use-package go-mode
-  :ensure t)
+  :ensure t
+  :config (add-hook 'go-mode-hook
+                    (lambda ()
+                      (setq tab-width 4))))
+
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (go-mode . lsp)
+         ;; if you want which-key integration
+         )
+  :commands lsp)

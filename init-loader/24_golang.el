eb44a805dc340b6f638e15e092134980ab9bc9ec
Jeongsoo Park <toracle@gmail.com>
2024-02-18 22:19:27 +0900

    install gopls if it is not ecists

--- init-loader/24_golang.el
+++ init-loader/24_golang.el
@@ -6,15 +6,5 @@
 
 ;; need to install gopls
 (unless (executable-find "gopls")
-  (message "Need to install gopls: go install golang.org/x/tools/gopls@latest"))
-
-
-;; (use-package lsp-mode
-;;   :init
-;;   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-;;   (setq lsp-keymap-prefix "C-c l")
-;;   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-;;          (go-mode . lsp)
-;;          ;; if you want which-key integration
-;;          )
-;;   :commands lsp)
+  (message "Need to install gopls: go install golang.org/x/tools/gopls@latest")
+  (shell-command "go install golang.org/x/tools/gopls@latest"))

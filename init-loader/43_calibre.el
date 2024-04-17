96adeb1c7cb5be5aa807c1c773c84ef9004b1acd
toracle <toracle@gmail.com>
2024-04-18 00:46:06 +0900

    set onedrive directory location using env variable if possible

--- init-loader/43_calibre.el
+++ init-loader/43_calibre.el
@@ -1,6 +1,7 @@
 (use-package calibredb
   :ensure t
   :config (progn
-            (setq calibredb-root-dir "~/OneDrive/Calibre Library")
+            (setq calibredb-root-dir (let ((loc (getenv "OneDriveConsumer")))
+                                       (if loc loc "~/OneDrive/Calibre Library")))
             (setq calibredb-db-dir (expand-file-name "metadata.db" calibredb-root-dir))
             (setq calibredb-id-width 6)))

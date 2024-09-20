bd4b2615de18e3f75fce6b91647e30f714b4d4d9
Jeongsoo Park <toracle@gmail.com>
2024-09-20 13:09:44 +0900

    touch cloud-only-store file before open with calibredb to download it to local

--- init-loader/43_calibre.el
+++ init-loader/43_calibre.el
@@ -1,5 +1,15 @@
+(defun calibredb-find-file-with-cloud-file (orig-func &rest args)
+  (let* ((file-path (cadr (assoc :file-path (caar args))))
+         (filename (expand-file-name file-path))
+         (cmd (concat "file \"" filename "\"")))
+    (shell-command cmd)
+    (apply orig-func args)))
+
+
 (use-package calibredb
   :ensure t
   :config (progn
             (setq calibredb-root-dir "~/OneDrive/Calibre Library")
-            (setq calibredb-db-dir (expand-file-name "metadata.db" calibredb-root-dir))))
+            (setq calibredb-db-dir (expand-file-name "metadata.db" calibredb-root-dir))
+            (when (eq system-type 'darwin)
+              (advice-add 'calibredb-find-file :around #'calibredb-find-file-with-cloud-file))))

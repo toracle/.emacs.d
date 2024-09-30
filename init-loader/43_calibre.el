9245f475bcb9ab7cc3e2d9541c1ee0d4fdb274fe
toracle <toracle@gmail.com>
2024-10-01 00:57:19 +0900

    expand user home directory

--- init-loader/43_calibre.el
+++ init-loader/43_calibre.el
@@ -2,6 +2,7 @@
   :ensure t
   :config (progn
             (setq calibredb-root-dir (let ((loc (getenv "OneDriveConsumer")))
-                                       (if loc loc "~/OneDrive/Calibre Library")))
+                                       (if loc (expand-file-name "Calibre Library" loc)
+                                         "~/OneDrive/Calibre Library")))
             (setq calibredb-db-dir (expand-file-name "metadata.db" calibredb-root-dir))
             (setq calibredb-id-width 6)))

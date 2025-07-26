427cdefc2a74e855f5229e731a9e337581ee28a6
Jeongsoo Park <toracle@gmail.com>
2025-07-27 00:10:43 +0900

    remove duplicated chatgpt-shell config, add claude-code-ide

--- init-loader/32_chatgpt.el
+++ init-loader/32_chatgpt.el
@@ -4,3 +4,10 @@
   ((chatgpt-shell-openai-key
     (lambda ()
       (auth-source-pick-first-password :host "api.openai.com")))))
+
+
+(when (functionp 'use-package-vc-install)
+  (use-package claude-code-ide
+    :vc (:url "https://github.com/manzaltu/claude-code-ide.el" :branch "main")
+    :bind ("C-c C-SPC" . claude-code-ide-menu)
+    :config (claude-code-ide-emacs-tools-setup)))

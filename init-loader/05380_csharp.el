76fbebfae13f9c701b925795805446857e2d18ad
toracle <toracle@gmail.com>
2026-02-24 08:57:42 +0900

    reorganize config

--- init-loader/05380_csharp.el
+++ init-loader/05380_csharp.el
@@ -1,39 +1,3 @@
-;;; 14_0_lang.el -- language configuration
-
-
-(use-package eglot
-  :ensure t)
-
-(use-package corfu
-  :ensure t
-  :init (global-corfu-mode)
-  :config (progn (setq corfu-auto t)))
-
-(use-package corfu-terminal
-  :ensure t
-  :init (unless (display-graphic-p)
-          (corfu-terminal-mode +1)))
-
-(use-package cape
-  :ensure t)
-
-(require 'treesit)
-(if (treesit-available-p)
-    t)
-
-(use-package flycheck
-  :ensure t
-  )
-
-(use-package epc
-  :ensure t)
-
-(use-package flycheck-inline
-  :ensure t)
-
-(use-package toml-mode
-  :ensure t)
-
 (use-package dotnet
   :ensure t)
 
@@ -70,5 +34,3 @@ folders relative to DIR-OR-FILE."
 
 (use-package sharper
   :ensure t)
-
-(provide '14_0_lang)
--- init-loader/07000_calibre.el
+++ init-loader/07000_calibre.el
@@ -1,3 +1,5 @@
+(require 'cl-lib)
+
 (defun calibredb-find-file-with-cloud-file (orig-func &rest args)
   (let* ((file-path (cadr (assoc :file-path (caar args))))
          (filename (expand-file-name file-path))
@@ -10,9 +12,9 @@
   :ensure t
   :config (progn
             (setq calibredb-root-dir 
-                  (case system-type
-                    ('darwin "~/OneDrive/Calibre Library")
-                    ('windows-nt (let ((loc (getenv "OneDriveConsumer")))
+                  (cl-case system-type
+                    (darwin "~/OneDrive/Calibre Library")
+                    (windows-nt (let ((loc (getenv "OneDriveConsumer")))
                                        (if loc (expand-file-name "Calibre Library" loc)
                                          "~/OneDrive/Calibre Library")))))
             (setq calibredb-db-dir (expand-file-name "metadata.db" calibredb-root-dir))
